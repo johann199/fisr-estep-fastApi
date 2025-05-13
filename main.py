925665141bc67320d328725a0e29c6db5bb44136
johann199 <johanngonzakez@gmail.com>
2025-05-13 14:30:28 -0500

    despliegue

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from user_jwt import crearToken
 from bd.database import  engine, Base
 from routers.movie import routerMovie
 from routers.user import routerUser
+import os
+import uvicorn
 
 app = FastAPI(
     title="My FastAPI Application",
@@ -24,3 +26,6 @@ Base.metadata.create_all(bind=engine)
 def read_root():
     return HTMLResponse('<h2>Welcome to My FastAPI Application</h2><p>This is a simple FastAPI application.</p>')
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0", port=port)
